07baa4d7ba70ed512c6a77678fcdeff0084a4c97
Don McClintock <don@ndc.nu>
2019-03-12 09:03:54 -0400

    Update Commands.sh

--- Commands.sh
+++ Commands.sh
@@ -10,8 +10,14 @@ grep 'bash_aliases' ~/.bash*  # check if it is in there...
 # ~/.bash_aliases, instead of adding them here directly.
 # add this to your .bashrc
 
+# ~/.bash_aliases ...
 if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
+fi
+
+# ~/.functions too ...
+if [ -f ~/.functions ]; then
+    . ~/.functions
 fi
 
 # THEN: - just download them with wget
@@ -25,6 +31,12 @@ if [ -f ~/.bash_logout ]; then
     mv ~/.bash_logout ~/.bash_logout.original
     cd ~
     wget https://raw.githubusercontent.com/netdesignate/dotfiles/master/.bash_logout
+fi
+
+if [ -f ~/.functions ]; then
+    mv ~/.functions ~/.functions.original
+    cd ~
+    wget https://raw.githubusercontent.com/netdesignate/dotfiles/master/.functions
 fi
 
 wget https://raw.githubusercontent.com/netdesignate/dotfiles/master/.bash_aliases
